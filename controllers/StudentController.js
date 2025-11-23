bd229d7c3eb728b7926890444839bc48d7f5e9ab
rcdo-dev <rcdo.dev@gmail.com>
2025-11-23 15:13:48 -0300

    feat: Function to implement students performed.

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -3,13 +3,23 @@ import * as StudentService from '../services/StudentService.js'
 export async function insertStudent(req, res) {
     try {
         const student = await StudentService.createStudent(req.body);
-        if(student){
+        if (student) {
             res.status(201).json(student);
-        }else{
-            res.status(400).json({error:'Failed to create student.'})
+        } else {
+            res.status(400).json({ error: 'Failed to create student.' })
         }
     } catch (error) {
-        console.error(error.message);
+        console.error('Error registering student.', error.message);
         res.status(500).json({ error: 'Internal server error.' });
+    }
+}
+
+export async function getAllStudents(res) {
+    try {
+        const students = await StudentService.readAllStudents();
+        res.status(200).json(students);
+    } catch (error) {
+        console.error('Error retrieving all students:', error.message)
+        res.status(500).json({ error: 'Internal server error.' })
     }
 }
