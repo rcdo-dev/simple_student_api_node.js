294d1b71dbb974598a7ef4658d0cfc8b056629ad
rcdo-dev <rcdo.dev@gmail.com>
2025-11-23 16:27:29 -0300

    feat: Routes implemented.

--- routes/StudentRoute.js
+++ routes/StudentRoute.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import {
     insertStudent,
+    getAllStudents,
+    getStudentById,
+    modifyStudent,
+    removeStudent
 } from '../controllers/StudentController.js'
 
 const router = Router();
 
 router.post('/alunos', insertStudent);
+router.get('/alunos', getAllStudents);
+router.get('/alunos/:id', getStudentById);
+router.put('/alunos/:id', modifyStudent); 
+router.delete('/alunos/:id', removeStudent);
 
 export default router;
