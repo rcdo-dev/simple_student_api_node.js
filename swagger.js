f224674afeb46cd01644d3714c3ee7c89cd62429
rcdo-dev <rcdo.dev@gmail.com>
2025-11-25 08:57:35 -0300

    feat: swagger file created.

---
+++ swagger.js
@@ -0,0 +1,18 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Students API',
+            version: '1.0.0',
+            description: 'API documentada usando Swagger'
+        },
+    },
+    apis: ['./routes/StudentRoute.js']
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export default { swaggerUi, swaggerSpec }
